c1edf2250285cf7aaca2c602f21be359e202c42b
Will Cheng <willchengsters@gmail.com>
2018-03-16 14:59:02 -0700

    react-intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+const luckyNumber = ()=> {
+    return Math.floor(Math.random()*1000)+1
+}
+
+const Greeting = (name, luckyNumber)=>{ 
+    return <div className = 'container'>
+        <h1> Welcome {name}</h1>
+        <h2> Your Lucky number is: {luckyNumber}</h2>
+    </div>
+ }
+
+const user = {
+    name: 'will',
+    luckyNumber: luckyNumber(),
+}
+
+
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <div className = "app">
+        {Greeting(user.name, user.luckyNumber)}
+    </div>,
     document.getElementById('root')
 );
